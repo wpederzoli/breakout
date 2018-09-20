41b55921f1e54a7f84b39fdf20592c717a8d235e
William <wpederzoli@gmail.com>
2018-09-21 00:52:00 +0200

    Name highscore bug fixed

---
+++ break12/src/Paddle.lua
@@ -0,0 +1,64 @@
+--[[
+    Represents a paddle that can move left and right. Used in the main
+    program to deflect the ball toward the bricks; if the ball passes
+    the paddle, the player loses one heart. The Paddle can have a skin,
+    which the player gets to choose upon starting the game.
+]]
+
+Paddle = Class{}
+
+-- Initialize paddle in the bottom-middle of map
+function Paddle:init()
+    -- x is placed in the middle
+    self.x = VIRTUAL_WIDTH / 2 - 32
+
+    -- y is placed a little above the bottom
+    self.y = VIRTUAL_HEIGHT - 32
+
+    --start with no velocity
+    self.dx = 0
+
+    --dimensions
+    self.width = 64
+    self.height = 16
+
+    --skin color
+    self.skin = 1
+
+    --paddle size
+    self.size = 2
+end
+
+function Paddle:update(dt)
+    --keyboard input
+    if love.keyboard.isDown('left') then
+        self.dx = -PADDLE_SPEED
+    elseif love.keyboard.isDown('right') then
+        self.dx = PADDLE_SPEED
+    else
+        self.dx = 0
+    end
+
+    -- math.max here ensures that we're the greater of 0 or the player's
+    -- current calculated Y position when pressing up so that we don't
+    -- go into the negatives; the movement calculation is simply our
+    -- previously-defined paddle speed scaled by dt
+    if self.dx < 0 then
+        self.x = math.max(0, self.x + self.dx * dt)
+    -- similar to before, this time we use math.min to ensure we don't
+    -- go any farther than the bottom of the screen minus the paddle's
+    -- height (or else it will go partially below, since position is
+    -- based on its top left corner)
+    else
+        self.x = math.min(VIRTUAL_WIDTH - self.width, self.x + self.dx * dt)
+    end
+end
+
+--[[
+    Render the paddle by drawing the main texture, passing in the quad
+    that corresponds to the proper skin and size.
+]]
+function Paddle:render()
+    love.graphics.draw(gTextures['main'], gFrames['paddles'][self.size + 4 * (self.skin - 1)],
+        self.x, self.y)
+end
