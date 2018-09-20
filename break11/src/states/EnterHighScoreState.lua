433a1f0297af3a0e2e288dfd422361749a913c07
William <wpederzoli@gmail.com>
2018-09-21 00:00:44 +0200

    High score input added

---
+++ break11/src/states/EnterHighScoreState.lua
@@ -0,0 +1,94 @@
+EnterHighScoreState = Class{__includes = BaseState}
+
+-- individual chars of string
+local chars = {
+    [1] = 65,
+    [2] = 65,
+    [3] = 65
+}
+
+-- char currently changing
+local highlightedChar = 1
+
+function EnterHighScoreState:enter(params)
+    self.highScores = params.highScores
+    self.score = params.score
+    self.scoreIndex = params.scoreIndex
+end
+
+function EnterHighScoreState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        --update scores table
+        local name = string.char(chars[1]) .. string.char(chars[2]) .. string.char(chars[3])
+
+        --go backwards through high scores table till this score, shifting scores
+        for i = 10, self.scoreIndex, -1 do
+            self.highScores[i + 1] = {
+                name = self.highScores[i].name,
+                score = self.highScores[i].score
+            }
+        end
+
+        self.highScores[self.scoreIndex].name = name
+        self.highScores[self.scoreIndex].score = self.score
+
+        --write scores to file
+        local scoresStr = ''
+
+        for i = 1, 10 do
+            scoresStr = scoresStr .. self.highScores[i].name .. '\n'
+            scoresStr = scoresStr .. tostring(self.highScores[i].score) .. '\n'
+        end
+
+        love.filesystem.write('breakout.lst', scoresStr)
+
+        gStateMachine:change('high-scores', {
+            highScores = self.highScores
+        })
+    end
+
+    --scroll through characters
+    if love.keyboard.wasPressed('up') then
+        chars[highlightedChar] = chars[highlightedChar] + 1
+        if chars[highlightedChar] > 90 then
+            chars[highlightedChar] = 65
+        end
+    elseif love.keyboard.wasPressed('down') then
+        chars[highlightedChar] = chars[highlightedChar] - 1
+        if chars[highlightedChar] < 65 then
+            chars[highlightedChar] = 90
+        end
+    end
+end
+
+function EnterHighScoreState:render()
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.printf('Your score: ' .. tostring(self.score), 0, 30,
+        VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(gFonts['large'])
+
+    -- render all three characters of the name
+
+    if highlightedChar == 1 then 
+        love.graphics.setColor(100, 255, 255)
+    end
+    love.graphics.print(string.char(chars[1]), VIRTUAL_WIDTH / 2 - 28, VIRTUAL_HEIGHT / 2)
+    love.graphics.setColor(255, 255, 255)
+
+    if highlightedChar == 2 then 
+        love.graphics.setColor(100, 255, 255)
+    end
+    love.graphics.print(string.char(chars[2]), VIRTUAL_WIDTH / 2 - 28, VIRTUAL_HEIGHT / 2)
+    love.graphics.setColor(255, 255, 255)
+
+    if highlightedChar == 3 then 
+        love.graphics.setColor(100, 255, 255)
+    end
+    love.graphics.print(string.char(chars[3]), VIRTUAL_WIDTH / 2 - 28, VIRTUAL_HEIGHT / 2)
+    love.graphics.setColor(255, 255, 255)
+
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.printf('Press Enter to confirm!', 0, VIRTUAL_HEIGHT - 18,
+        VIRTUAL_WIDTH, 'center')
+end
---
+++ break11/src/states/GameOver.lua
@@ -0,0 +1,57 @@
+--[[
+    When the ball passes the bottom border and the lives
+    are 0 a Game Over message and the score should be 
+    displayed in the middle of the screen 
+]]
+
+GameOver = Class{__includes = BaseState}
+
+function GameOver:enter(params)
+    self.score =  params.score
+    self.highScores = params.highScores
+end
+
+function GameOver:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        --see if score is higher than any in high scores table
+        local highScore = false
+
+        --keep track of what high scores this one overwrites, if any
+        local highScoreIndex = 11
+
+        for i = 10, 1, -1 do
+            local score = self.highScores[i].score or 0
+            if self.score > score then
+                highScoreIndex = i
+                highScore = true
+            end
+        end
+
+        if highScore then
+            gSounds['high-score']:play()
+            gStateMachine:change('enter-high-score', {
+                highScores = self.highScores,
+                score = self.score,
+                scoreIndex = highScoreIndex
+            })
+        else
+            gStateMachine:change('start', {
+                highScores = self.highScores
+            })
+        end
+    end
+
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function GameOver:render()
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf('GAME OVER', 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.printf('Final Score: ' .. tostring(self.score), 0, VIRTUAL_HEIGHT / 2,
+        VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter!', 0, VIRTUAL_HEIGHT - VIRTUAL_HEIGHT / 4,
+        VIRTUAL_WIDTH, 'center')
+end
